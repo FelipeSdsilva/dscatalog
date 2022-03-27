3828e732e688b5b69f06a6222251e342133b77f7
Felipe Sousa <felipe.fps09@hotmail.com>
2022-03-27 13:15:57 -0300

    Implement class DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,22 +1,33 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		//This form is lambda convert entity in DTO
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		/* this is formal convert object in DTO
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for(Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		*/
+		
 	}
 }
