606fe3d64bdfc178acb422d5ce35dee06d86be7a
Felipe Sousa <felipe.fps09@hotmail.com>
2022-03-26 22:51:07 -0300

    Category resource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+
+	public Category() {
+
+	}
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
