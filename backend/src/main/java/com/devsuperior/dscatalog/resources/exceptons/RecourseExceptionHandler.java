68b9d70029a0d575031d876098af650e2a5e58f0
Felipe Sousa <felipe.fps09@hotmail.com>
2022-03-28 15:27:04 -0300

    Exception handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptons/RecourseExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.resources.exceptons;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class RecourseExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+}
