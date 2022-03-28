265e15a600272b45074ed163861f7b1266a4c505
Felipe Sousa <felipe.fps09@hotmail.com>
2022-03-28 19:53:03 -0300

    Insert Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -37,4 +37,12 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO inset(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 }
