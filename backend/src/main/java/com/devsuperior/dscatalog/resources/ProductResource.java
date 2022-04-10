0ebba69cdcf597f38c971fce372c99a3008358a3
Felipe Sousa <felipe.fps09@hotmail.com>
2022-04-10 09:35:31 -0300

    First test in product repository deletebyid

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -4,9 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,28 @@
+ package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+@DataJpaTest
+ public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExist() {
+		
+		long exintingId = 1L;
+		
+		repository.deleteById(exintingId);
+		
+		Optional<Product> result = repository.findById(exintingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+}
