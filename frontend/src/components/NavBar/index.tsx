588ea6855ceaf33c9f29b85fffffacf076251ac1
Felipe Sousa <felipe.fps09@hotmail.com>
2022-10-12 09:48:38 -0300

    Menu toggle

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,17 +1,32 @@
-import  './styles.css';
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-md bg-primary">
-        <div className='container-fluid'>
-          <a href="link" className='nav-logo-text'>
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+        <div className="container-fluid">
+          <a href="link" className="nav-logo-text">
             <h4>DS Catalog</h4>
           </a>
-          <div className='collapse navbar-collapse'>
-            <ul className='navbar-nav offset-md-2 main-menu'>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#dscatalog-navbar"
+            aria-controls="dscatalog-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="dscatalog-navbar">
+            <ul className="navbar-nav offset-md-2 main-menu">
               <li>
-                <a href="link">HOME</a>
+                <a href="link" className="active">
+                  HOME
+                </a>
               </li>
               <li>
                 <a href="link">CATÁLOGO</a>
