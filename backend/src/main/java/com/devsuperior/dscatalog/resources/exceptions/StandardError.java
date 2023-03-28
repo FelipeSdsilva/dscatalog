2ecdcd9253c5c5d2412d09e9f7e746a04187d9e5
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-28 17:40:53 -0300

    Update class StandardError with ,lombok implementation

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -1,58 +1,22 @@
 package com.devsuperior.dscatalog.resources.exceptions;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.time.Instant;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class StandardError implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private Instant timestamp;
-	private Integer status;
-	private String error;
-	private String message;
-	private String path;
+    private static final long serialVersionUID = 1L;
 
-	public StandardError() {
-	}
-
-	public Instant getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Instant timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-	public String getError() {
-		return error;
-	}
-
-	public void setError(String error) {
-		this.error = error;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
 
 }
